2a6109af733c0b5c091964abec73baed8d96cfe9
Kramer Brown <brownkramer63@gmail.com>
2022-09-14 12:47:23 -0500

    practice

---
+++ src/CodingBat/math1.java
@@ -0,0 +1,14 @@
+package CodingBat;
+
+import java.util.List;
+
+public class math1 {
+    public static void main(String[] args) {
+
+    }
+    public List<Integer> math1(List<Integer> nums) {
+nums.replaceAll(n -> (n+1)*10);
+
+        return nums;
+    }
+}
