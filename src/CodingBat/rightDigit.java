e914e19d3c5474e0b4f275b10957e8e75ebc9318
Kramer Brown <brownkramer63@gmail.com>
2022-09-14 18:02:15 -0500

    practice

---
+++ src/CodingBat/rightDigit.java
@@ -0,0 +1,15 @@
+package CodingBat;
+
+import java.util.List;
+
+public class rightDigit {
+    public static void main(String[] args) {
+
+    }
+    public List<Integer> rightDigit(List<Integer> nums) {
+
+
+
+        return nums;
+    }
+}
