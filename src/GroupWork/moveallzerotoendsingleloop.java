238a927595b450cf10e7aa2cbabafe62be18e671
Kramer Brown <brownkramer63@gmail.com>
2022-09-21 19:17:34 -0500

    practice

---
+++ src/GroupWork/moveallzerotoendsingleloop.java
@@ -0,0 +1,33 @@
+package GroupWork;
+
+import java.util.Arrays;
+
+public class moveallzerotoendsingleloop {
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 2, 0, 0, 7,8,0};
+        System.out.println(Arrays.toString(moveallzero(arr)));
+    }
+
+    public static int[] moveallzero(int[] array) {
+
+        int fast = 0;
+        int slow = 0;
+
+        while (fast < array.length - 1) {
+
+            if (array[slow] != 0) {
+                slow++;
+            }
+            if (fast <= slow || array[fast] == 0) {
+                fast++;
+            }
+
+            if (array[slow] == 0 && array[fast] != 0) {
+
+                array[slow] = array[fast];
+                array[fast] = 0;
+            }
+        }
+        return array;
+    }
+}
