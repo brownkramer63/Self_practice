4c0797938da5dba397addb2bd2cc3b10b153abbb
Kramer Brown <brownkramer63@gmail.com>
2022-09-26 20:05:41 -0500

    practice

---
+++ src/CodingBat919/sum13.java
@@ -0,0 +1,10 @@
+package CodingBat919;
+
+public class sum13 {
+    public static void main(String[] args) {
+int[] nums = {1, 2, 2, 1, 13};
+//        System.out.println(sum13(nums));
+
+    }
+
+}
