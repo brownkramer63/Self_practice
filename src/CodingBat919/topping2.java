ac9125b99cd1f177b90f953cc673eadcf171470e
Kramer Brown <brownkramer63@gmail.com>
2022-10-02 13:02:35 -0500

    practice

--- src/CodingBat919/topping2.java
+++ src/CodingBat919/topping2.java
@@ -8,7 +8,14 @@ public class topping2 {
     }
 
     public Map<String, String> topping2(Map<String,String> map){
-      map.containsKey("kramer","kramer");
+     if (map.containsKey("ice cream")){
+         map.put("yogurt",map.get("ice cream"));
+     }
+     if (map.containsKey("spinach")){
+
+         map.put("spinach","nuts");
+
+     }
 
       return map;
       //other commits not happening
