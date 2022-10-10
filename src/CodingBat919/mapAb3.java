b5a0788c09678ec53d1cc185a2d330109bbba6af
Kramer Brown <brownkramer63@gmail.com>
2022-10-10 18:05:14 -0500

    self practice

---
+++ src/CodingBat919/mapAb3.java
@@ -0,0 +1,21 @@
+package CodingBat919;
+
+import java.util.Map;
+
+public class mapAb3 {
+    public static void main(String[] args) {
+
+    }
+    public Map<String, String> mapAB3(Map<String, String> map) {
+if (map.containsKey("a") && (!map.containsKey("b"))){
+    map.put("b",map.get("a"));
+}
+        if (map.containsKey("b") && (!map.containsKey("a"))){
+            map.put("a",map.get("b"));
+        }
+        return map;
+
+
+
+    }
+}
