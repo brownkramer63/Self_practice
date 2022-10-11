0f6069a5ff1521f5cdd963cdbc49b88e906434d7
Kramer Brown <brownkramer63@gmail.com>
2022-10-11 16:44:50 -0500

    self practice

--- src/GroupWork/IntersectionofTwoLinkedLists.java
+++ src/GroupWork/IntersectionofTwoLinkedLists.java
@@ -2,7 +2,10 @@ package GroupWork;
 
 public class IntersectionofTwoLinkedLists {
     public static void main(String[] args) {
-
+//need to make linked lists here
+        Link linkedlist1 = new Link(4);
     }
+
+
 
 }
---
+++ src/GroupWork/Link.java
@@ -0,0 +1,12 @@
+package GroupWork;
+
+public class Link {
+    public int data;
+    public Link next;
+
+//i dont really get this
+    public Link(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
