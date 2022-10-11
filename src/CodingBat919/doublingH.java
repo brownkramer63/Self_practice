d3068a3b9506f7993a9e66703befd1b5c0171a51
Kramer Brown <brownkramer63@gmail.com>
2022-10-11 16:49:50 -0500

    self practice

---
+++ src/CodingBat919/doublingH.java
@@ -0,0 +1,14 @@
+package CodingBat919;
+
+import java.util.List;
+
+public class doublingH {
+    public static void main(String[] args) {
+
+    }
+    public List<Integer> doubling(List<Integer> nums) {
+nums.replaceAll(n-> n*2);
+return nums;
+    }
+
+}
