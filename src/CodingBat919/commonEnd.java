459238280f30b0f7359c4d761e80aaacce62f331
Kramer Brown <brownkramer63@gmail.com>
2022-10-13 17:50:29 -0500

    self practice

---
+++ src/CodingBat919/commonEnd.java
@@ -0,0 +1,13 @@
+package CodingBat919;
+
+public class commonEnd {
+    public static void main(String[] args) {
+
+    }
+    public boolean commonEnd(int[] a, int[] b) {
+        if(a[0]==b[0] || a[a.length-1]==b[b.length-1]){
+            return true;
+        }
+        return false;
+    }
+}
--- src/CodingBat919/sum13.java
+++ src/CodingBat919/sum13.java
@@ -3,8 +3,20 @@ package CodingBat919;
 public class sum13 {
     public static void main(String[] args) {
 int[] nums = {1, 2, 2, 1, 13};
-//        System.out.println(sum13(nums));
 
+    }
+    public int sum13(int[] nums) {
+        int sum=0;
+
+        for (int i = 0; i <nums.length ; i++) {
+            if (nums[i]!=13){
+                sum+=nums[i];
+            }
+            if (nums[i]==13){
+                i++;
+            }
+        }
+        return sum;
     }
 
 }
