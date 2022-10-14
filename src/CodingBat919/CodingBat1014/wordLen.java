10ca4218267cced90402c94d1bb00b1d56c8cddf
Kramer Brown <brownkramer63@gmail.com>
2022-10-14 10:51:19 -0500

    self practice

---
+++ src/CodingBat919/CodingBat1014/wordLen.java
@@ -0,0 +1,24 @@
+package CodingBat919.CodingBat1014;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class wordLen {
+    public static void main(String[] args) {
+
+    }
+    public Map<String, Integer> wordLen(String[] strings) {
+
+        HashMap<String,Integer>map2 = new HashMap<>();
+
+        for (String each:strings
+             ) {
+         if (!map2.containsKey(each)){
+             map2.put(each,each.length());
+         }
+        }
+
+    return map2;
+    }
+
+}
