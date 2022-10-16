31f31f4f098e8ab9a347c6f026a4b765c4019971
Kramer Brown <brownkramer63@gmail.com>
2022-10-16 11:02:17 -0500

    self practice

---
+++ src/CodingBat919/CodingBat1014/firstChar.java
@@ -0,0 +1,27 @@
+package CodingBat919.CodingBat1014;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class firstChar {
+    public static void main(String[] args) {
+
+    }
+    public Map<String, String> firstChar(String[] strings) {
+Map<String, String >Map3 = new HashMap<>();
+String empty="";
+//i need to check each string againest all other strings im thinking nested loop
+        for (String each:strings
+             ) {
+            for (String each2:strings
+                 ) {
+                if (each2.charAt(0)==each.charAt(0) && (!empty.contains(each))){
+                    empty+=each2+each;
+                }
+            }
+            Map3.put(each.charAt(0)+"",empty);
+            empty="";
+        }
+        return Map3;
+    }
+}
