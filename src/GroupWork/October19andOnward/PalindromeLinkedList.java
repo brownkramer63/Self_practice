a3c4a1ae9b7f59a309171db94020e3c8e913b9db
Kramer Brown <brownkramer63@gmail.com>
2022-10-19 13:14:16 -0500

    groupwork

---
+++ src/GroupWork/October19andOnward/PalindromeLinkedList.java
@@ -0,0 +1,64 @@
+package GroupWork.October19andOnward;
+
+import GroupWork.Link;
+import GroupWork.Node;
+
+import java.util.Stack;
+
+public class PalindromeLinkedList {
+    //our link class with constructor
+    public class Node01 {
+        public int data;
+        public Node01 next;
+
+        public Node01(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    //our main class with linked list we will make
+    public static void main(String[] args) {
+
+//        Node01 one = new Node01(1);
+//        Node01 two= new Node01(2);
+//        Node01 three = new Node01(3);
+//        Node01 four= new Node01(2);
+//        Node01 five = new Node01(1);
+//
+//        one.next=two;
+//        two.next=three;
+//        three.next=four;
+//        four.next=five;
+//        System.out.println(isPalindromeLL(one));
+
+
+    }
+//formula we will write using stack
+static boolean isPalindromeLL(Node01 head){
+Node01 slow = head;
+boolean ispalindrome= true;
+    Stack<Integer> stack01 = new Stack<>();
+
+    //adding data to our stack
+    while (slow!=null){
+        stack01.push(slow.data);
+        slow=slow.next;
+    }
+    //pulling from stack which would be in reverse order and comparing to LL using pop
+    //then moving onto next LL element if not a palindrome breaks and returns false
+    while (head!=null){
+       int i = stack01.pop();
+       if (head.data==i){
+           ispalindrome=true;
+       }else {
+           ispalindrome=false;
+           break;
+       }
+       head=head.next;
+
+    }
+return ispalindrome;
+}
+
+}
---
+++ src/GroupWork/October19andOnward/SingleNumber.java
@@ -0,0 +1,41 @@
+package GroupWork.October19andOnward;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SingleNumber {
+    public static void main(String[] args) {
+    int[] nums ={4,1,2,1,2};
+        System.out.println(singleNumber(nums));
+
+
+    }
+    public static int singleNumber(int[] nums){
+        int numslength= nums.length;
+        Map<Integer, Integer>map1 = new HashMap<>();
+        //simply making the hash map and adding in the nums[i] as key and amount they show up as value in count
+        for (int i = 0; i <numslength ; i++) {
+            Integer count= map1.get(nums[i]);
+
+            if (count==null){
+                count=1;
+            }
+            else {
+                count++;
+            }
+            map1.put(nums[i],count);
+        }
+        //returning the value that is not a duplicate
+        for (Integer key:nums
+             ) {
+            if (map1.get(key)!=2){
+                return key;
+            }
+        }
+
+
+
+
+        return 911;
+    }
+}
