151a481a78d69a79b0ecf019091154943bc6652c
Kramer Brown <brownkramer63@gmail.com>
2022-10-21 12:13:57 -0500

    selfpractice

--- src/CodingBatAP1/ScoresIncreasing.java
+++ src/CodingBatAP1/ScoresIncreasing.java
@@ -3,10 +3,11 @@ package CodingBatAP1;
 public class ScoresIncreasing {
     public static void main(String[] args) {
 
+        int[] score={1,1,2,4,4,7};
     }
     public boolean scoresIncreasing(int[] scores) {
         for (int i = 1; i <scores.length ; i++) {
-            if (!(scores[i]>scores[i-1])){
+            if (!(scores[i]>=scores[i-1])){
                return false;
             }
         }
