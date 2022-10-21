800622408627d10af1c2f73c4808b9e1eaffafca
Kramer Brown <brownkramer63@gmail.com>
2022-10-21 12:24:48 -0500

    selfpractice

---
+++ src/CodingBatAP1/scores100.java
@@ -0,0 +1,25 @@
+package CodingBatAP1;
+
+public class scores100 {
+    public static void main(String[] args) {
+
+
+    }
+    public boolean scores100(int[] scores) {
+        boolean answer= false;
+        for (int i = 1; i < scores.length-1 ; i++) {
+           if (scores[i]==100){
+               if (scores[i-1]==100){
+                  answer=true;
+                  break;
+               }
+               if (scores[i+1]==100){
+                   answer=true;
+                   break;
+               }
+
+           }
+        }
+        return answer;
+    }
+}
