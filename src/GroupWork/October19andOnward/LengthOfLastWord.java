8ce41e66349b1d9a8cd6d47aef2f0275eae5e9ef
Kramer Brown <brownkramer63@gmail.com>
2022-11-02 16:04:45 -0500

    selfpractice

---
+++ src/GroupWork/October19andOnward/LengthOfLastWord.java
@@ -0,0 +1,13 @@
+package GroupWork.October19andOnward;
+
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+
+        String word = " fly me to the moon ";
+    }
+    public String Lengthoflastword(String word){
+
+
+return null;
+    }
+}
