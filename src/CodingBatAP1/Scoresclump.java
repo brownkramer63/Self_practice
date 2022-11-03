1c7e495ee5277360e8c9b471e816e44a23f5015c
Kramer Brown <brownkramer63@gmail.com>
2022-11-03 15:40:28 -0500

    selfpractice

---
+++ src/CodingBatAP1/Scoresclump.java
@@ -0,0 +1,19 @@
+package CodingBatAP1;
+
+public class Scoresclump {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean scoresClump(int[] scores) {
+        int size = scores.length - 2;
+        for (int i = 1; i < size; i++) {
+            if (scores[i - 1] - scores[i] > 2 || scores[i + 1] - scores[i] > 2 || scores[i + 1] - scores[i+1] > 2) {
+                return false;
+            }
+
+
+        }
+        return true;
+    }
+}
