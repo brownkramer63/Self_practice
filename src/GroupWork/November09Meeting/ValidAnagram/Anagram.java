7eeb1a7dfd3403674cfefdd87f9e35d0b4ad48f3
Kramer Brown <brownkramer63@gmail.com>
2022-11-09 16:20:40 -0600

    selfpractice

--- src/GroupWork/November09Meeting/ValidAnagram/Anagram.java
+++ src/GroupWork/November09Meeting/ValidAnagram/Anagram.java
@@ -1,4 +1,25 @@
 package GroupWork.November09Meeting.ValidAnagram;
 
 public class Anagram {
+    public static void main(String[] args) {
+
+        System.out.println(ValidAnagramMethod("anagram","tagaram"));
+    }
+    public static boolean ValidAnagramMethod(String str1, String str2){
+     if (str1.length()!=str2.length()){
+         return false;
+     }
+
+        for (int i = 0; i <str2.length() ; i++) {
+            if (str1.contains(str2.charAt(i)+"")){
+                continue;
+            }
+            else {
+                return false;
+            }
+        }
+
+
+        return true;
+    }
 }
