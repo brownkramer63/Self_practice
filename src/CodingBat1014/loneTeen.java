368cb6e7f308766fb1a0c3e110232a4f0c7668a8
Kramer Brown <brownkramer63@gmail.com>
2022-11-10 22:11:15 -0600

    selfpractice

---
+++ src/CodingBat1014/loneTeen.java
@@ -0,0 +1,14 @@
+package CodingBat1014;
+
+public class loneTeen {
+    public static void main(String[] args) {
+
+    }
+    public static boolean loneTeam(int a, int b){
+
+
+        if (a>=13 )
+    return false;
+        return true;
+    }
+}
