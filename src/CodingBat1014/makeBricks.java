e9905f33fda6db97fe98323e12b94450ed1af0f8
Kramer Brown <brownkramer63@gmail.com>
2022-11-17 13:03:12 -0600

    selfpractice

---
+++ src/CodingBat1014/makeBricks.java
@@ -0,0 +1,18 @@
+package CodingBat1014;
+
+public class makeBricks {
+    public static void main(String[] args) {
+
+        System.out.println(makeBricks(3,2,10));
+    }
+    public static boolean makeBricks(int small, int big, int goal) {
+
+        int smallbricks = 1;
+        int bigbrick = 5;
+
+        int answer = 0;
+
+
+
+    }
+}
