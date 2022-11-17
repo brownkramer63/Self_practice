a20d6c143c8b104f33b71654fe64dc4c990f24f7
Kramer Brown <brownkramer63@gmail.com>
2022-11-17 13:12:55 -0600

    selfpractice

--- src/CodingBat1014/makeBricks.java
+++ src/CodingBat1014/makeBricks.java
@@ -3,7 +3,7 @@ package CodingBat1014;
 public class makeBricks {
     public static void main(String[] args) {
 
-        System.out.println(makeBricks(3,2,10));
+        System.out.println(makeBricks(3,2,9));
     }
     public static boolean makeBricks(int small, int big, int goal) {
 
@@ -12,7 +12,21 @@ public class makeBricks {
 
         int answer = 0;
 
+        if (goal-(smallbricks*small)==0){
+            return true;
+        }
+        if (goal-(big*bigbrick)==0){
+            return true;
+        }
+
+        answer=(small*smallbricks)+(big*bigbrick);
+        answer=goal-answer;
+        if (answer==0){
+            return true;
+        }
 
 
+
+return false;
     }
 }
