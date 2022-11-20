a5807eedc934ddbab1c388d88ad92e889d8c8b19
Kramer Brown <brownkramer63@gmail.com>
2022-11-20 15:22:36 -0600

    selfpractice

---
+++ src/GroupWork/November1116/Program.java
@@ -0,0 +1,12 @@
+package GroupWork.November1116;
+
+public class Program {
+    public static void main(String[] args)
+    {
+        System.out.print("Java");
+    }
+    public static void main(Integer[] arg)
+    {
+        System.out.print(123);
+    }
+}
