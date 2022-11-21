98b94696c9568d453e0d04475275909c4f2ccc72
Kramer Brown <brownkramer63@gmail.com>
2022-11-20 19:30:55 -0600

    selfpractice

---
+++ src/CodingBatFunctionalInterfaces/copies3.java
@@ -0,0 +1,15 @@
+package CodingBatFunctionalInterfaces;
+
+import java.util.List;
+
+public class copies3 {
+    public static void main(String[] args) {
+
+    }
+    public List<String> copies3(List<String> strings) {
+strings.replaceAll(n -> n+n+n);
+
+return strings;
+    }
+
+}
