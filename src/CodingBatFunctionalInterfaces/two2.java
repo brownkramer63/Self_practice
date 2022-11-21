ed7acf7d06649a79c8832649dc794240c6b89b32
Kramer Brown <brownkramer63@gmail.com>
2022-11-21 13:18:42 -0600

    selfpractice

---
+++ src/CodingBatFunctionalInterfaces/two2.java
@@ -0,0 +1,14 @@
+package CodingBatFunctionalInterfaces;
+
+import java.util.List;
+
+public class two2 {
+    public static void main(String[] args) {
+
+    }
+    public List<Integer> two2(List<Integer> nums) {
+
+
+return nums;
+    }
+}
