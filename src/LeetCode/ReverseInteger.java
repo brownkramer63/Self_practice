5f297e817cf682c0d5287b2db21eff58a3d09156
Kramer Brown <brownkramer63@gmail.com>
2022-11-21 17:20:49 -0600

    selfpractice

---
+++ src/LeetCode/ReverseInteger.java
@@ -0,0 +1,36 @@
+package LeetCode;
+
+import java.util.Stack;
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+
+    }
+    public static int reverse(int x) {
+        if (x==0 ){
+            return 0;
+        }
+       int counter=1;
+        if (x<0){
+            counter=-1;
+        }
+        String str = Integer.toString(x);
+       char[] arr= str.toCharArray();
+       String holder = "";
+        Stack<Character> stacker = new Stack<>();
+        for (char each:arr
+             ) {
+            if (each!='0' && each!='-'){
+                stacker.push(each);
+            }
+
+        }
+        while (!stacker.isEmpty()){
+            holder=holder+stacker.pop();
+
+        }
+       int answer= Integer.parseInt(holder);
+      answer= answer*counter;
+      return answer;
+    }
+}
