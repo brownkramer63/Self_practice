312c12a5848a43c1ca95485166978e11a2e708fc
Kramer Brown <brownkramer63@gmail.com>
2022-11-30 16:11:13 -0600

    selfpractice

---
+++ src/GroupWork/November28/LowestoCommonAncestor.java
@@ -0,0 +1,11 @@
+package GroupWork.November28;
+
+public class LowestoCommonAncestor {
+    public static void main(String[] args) {
+
+    }
+    public static TreeNode lowestCommon(TreeNode node){
+
+        return node;
+    }
+}
--- src/GroupWork/November28/SumOfDigits.java
+++ src/GroupWork/November28/SumOfDigits.java
@@ -1,5 +1,13 @@
 package GroupWork.November28;
 
 public class SumOfDigits {
+    public static void main(String[] args) {
 
+    }
+   public static int digitalRootSum(int num){
+        int answer=0;
+
+
+        return answer;
+   }
 }
