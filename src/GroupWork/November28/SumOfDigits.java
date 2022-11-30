de92359f5a6feb8088743bb8e188d4017e2e006b
Kramer Brown <brownkramer63@gmail.com>
2022-11-30 16:34:48 -0600

    selfpractice

--- src/GroupWork/November28/SumOfDigits.java
+++ src/GroupWork/November28/SumOfDigits.java
@@ -2,12 +2,22 @@ package GroupWork.November28;
 
 public class SumOfDigits {
     public static void main(String[] args) {
-
+        digitalRootSum(132189);
     }
-   public static int digitalRootSum(int num){
+   public static void digitalRootSum(int num){
         int answer=0;
+   String[] arr = (num+"").split("");
+       for (String each:arr
+            ) {
+        answer= answer +  Integer.parseInt(each);
+       }
+       System.out.println(answer);
+       if (answer<=9){
 
-
-        return answer;
+        return;}
+       else {
+           num=answer;
+           digitalRootSum(num);
+       }
    }
 }
