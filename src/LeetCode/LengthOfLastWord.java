0febbc4a01ca344866f9782bb5be681d3a46101a
Kramer Brown <brownkramer63@gmail.com>
2022-12-01 13:29:42 -0600

    selfpractice

--- src/LeetCode/LengthOfLastWord.java
+++ src/LeetCode/LengthOfLastWord.java
@@ -7,28 +7,30 @@ public class LengthOfLastWord {
 
         System.out.println(lengthOfLastWord("   fly me   to   the moon  "));
     }
+
     public static int lengthOfLastWord(String s) {
         Stack<String> stack1 = new Stack<>();
         int count = 0;
-        int answer=0;
+        int answer = 0;
+        int loop = s.length() - 1;
 
-        for (int i = s.length(); i <s.length() ; i--) {
-            if (s.charAt(i)+""==" " && count==1){
+        while (count != 1) {
+            if ((!(s.charAt(loop) + "").equals(" ")) && (s.charAt(loop - 1) + "").equals(" ")) {
+                count++;
                 break;
             }
 
-            if (s.charAt(i)+""!=" " && s.charAt(i-1)+""!=" "){
-              stack1.push  (s.charAt(i)+"");
+            if (!(s.charAt(loop) + "").equals(" ")) {
+                stack1.push(s.charAt(loop) + "");
             }
-            if (s.charAt(i)+""!=" " && s.charAt(i-1)+""==" "){
-                stack1.push  (s.charAt(i)+"");
-                count++;
-            }
+        loop--;
         }
-       while (!stack1.isEmpty()){
-           stack1.pop();
-           answer++;
-       }
-return answer;
+        while (!stack1.isEmpty()) {
+            stack1.pop();
+            answer++;
+
+        }
+        return answer+1;
     }
+
 }
