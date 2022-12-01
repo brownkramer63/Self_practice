b053ce55e9753a3267479d58b9509d3cb0b1b71c
Kramer Brown <brownkramer63@gmail.com>
2022-11-30 18:41:15 -0600

    selfpractice

--- src/GroupWork/November28/LowestoCommonAncestor.java
+++ src/GroupWork/November28/LowestoCommonAncestor.java
@@ -1,10 +1,22 @@
 package GroupWork.November28;
 
 public class LowestoCommonAncestor {
+    static TreeNode getSampleTreeNode2(){
+        TreeNode root = new TreeNode(-10);
+        root.leftChild = new TreeNode(-15);
+        root.rightChild = new TreeNode(0);
+        root.leftChild.leftChild = new TreeNode(-22);
+        root.leftChild.rightChild = new TreeNode(-13);
+        root.leftChild.leftChild.leftChild = new TreeNode(-25);
+
+        return root;
+    }
+
     public static void main(String[] args) {
 
     }
     public static TreeNode lowestCommon(TreeNode node){
+
 
         return node;
     }
--- src/GroupWork/November28/SumOfDigits.java
+++ src/GroupWork/November28/SumOfDigits.java
@@ -4,20 +4,21 @@ public class SumOfDigits {
     public static void main(String[] args) {
         digitalRootSum(132189);
     }
-   public static void digitalRootSum(int num){
-        int answer=0;
-   String[] arr = (num+"").split("");
-       for (String each:arr
-            ) {
-        answer= answer +  Integer.parseInt(each);
-       }
-       System.out.println(answer);
-       if (answer<=9){
 
-        return;}
-       else {
-           num=answer;
-           digitalRootSum(num);
-       }
-   }
+    public static void digitalRootSum(int num) {
+        int answer = 0;
+        String[] arr = (num + "").split("");
+        for (String each : arr
+        ) {
+            answer = answer + Integer.parseInt(each);
+        }
+        System.out.println(answer);
+        if (answer <= 9) {
+
+            return;
+        } else {
+            num = answer;
+            digitalRootSum(num);
+        }
+    }
 }
