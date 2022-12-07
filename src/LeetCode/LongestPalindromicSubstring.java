cc386c9f19ee6f4640884d42ef5f59285c8f1ca5
Kramer Brown <brownkramer63@gmail.com>
2022-12-07 15:50:48 -0600

    selfpractice

---
+++ src/LeetCode/LongestPalindromicSubstring.java
@@ -0,0 +1,14 @@
+package LeetCode;
+
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        String s = "babad";
+        System.out.println(longestPalindrome(s));
+    }
+    public static String longestPalindrome(String s){
+
+return s;
+//couldnt figure out:(
+
+    }
+}
