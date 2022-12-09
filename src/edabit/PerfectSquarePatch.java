36a7ddfdfe87e7af74a8691504a954d3cb8e97a3
Kramer Brown <brownkramer63@gmail.com>
2022-12-09 13:52:05 -0600

    selfpractice

---
+++ src/edabit/PerfectSquarePatch.java
@@ -0,0 +1,22 @@
+package edabit;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PerfectSquarePatch {
+    public static void main(String[] args) {
+        int n =5;
+
+    }
+    public static int[][] squarePatch(int n) {
+
+        int[][] arr = new int[n][n];
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j <n ; j++) {
+                arr[i][j]=n;
+            }
+        }
+return arr;
+    }
+}
