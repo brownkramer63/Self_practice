f9543e39e3f3ec96bf991dc72f8a457657ed4a19
Kramer Brown <brownkramer63@gmail.com>
2022-12-21 14:24:29 -0600

    selfpractice

---
+++ src/edabit/ArraySum.java
@@ -0,0 +1,15 @@
+package edabit;
+
+public class ArraySum {
+    public static void main(String[] args) {
+
+    }
+    public static int sum(int[] arr) {
+int answer =0;
+        for (Integer each:arr
+             ) {
+            answer=answer+each;
+        }
+        return answer;
+    }
+}
