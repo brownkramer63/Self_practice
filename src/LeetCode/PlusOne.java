3004e64ebfc8539830e116632b6c865de063b339
Kramer Brown <brownkramer63@gmail.com>
2022-12-22 11:51:19 -0600

    selfpractice

--- src/LeetCode/PlusOne.java
+++ src/LeetCode/PlusOne.java
@@ -1,10 +1,33 @@
 package LeetCode;
 
+import java.util.Arrays;
+
 public class PlusOne {
     public static void main(String[] args) {
 
+        int[] digits={1,2,9};
+        int[] digits1={1,2,3};
+        System.out.println(Arrays.toString(plusOne(digits1)));
     }
-//    public int[] plusOne(int[] digits) {
-//return 0;
-//    }
+    public static int[] plusOne(int[] digits) {
+if (digits.length==1){
+    int[] answer={1,0};
+    if (digits[0]<9){
+        digits[0]=digits[0]+1;
+        return digits;
+    }else {
+        return answer;
+    }
+
+}
+
+        if (digits[digits.length-1]<9){
+            digits[digits.length-1]=digits[digits.length-1]+1;
+        }else {
+            digits[digits.length-1]=0;
+            digits[digits.length-2]=digits[digits.length-2]+1;
+        }
+
+return digits;
+    }
 }
