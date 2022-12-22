1c5eac3b6a293a700f887c3829fb659ce3d4c0d3
Kramer Brown <brownkramer63@gmail.com>
2022-12-22 11:54:57 -0600

    selfpractice

--- src/LeetCode/PlusOne.java
+++ src/LeetCode/PlusOne.java
@@ -30,4 +30,19 @@ if (digits.length==1){
 
 return digits;
     }
+//    leetcode solution below
+//    int n = digits.length;
+//    for(int i=n-1; i>=0; i--) {
+//        if(digits[i] < 9) {
+//            digits[i]++;
+//            return digits;
+//        }
+//
+//        digits[i] = 0;
+//    }
+//
+//    int[] newNumber = new int [n+1];
+//    newNumber[0] = 1;
+//
+//    return newNumber;
 }
