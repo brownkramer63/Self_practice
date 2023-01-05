59f28b6e01b8857c1ef8e5896542fe5a2bcda0ec
Kramer Brown <brownkramer63@gmail.com>
2023-01-05 15:11:50 -0600

    selfpractice

---
+++ src/LeetCode/RemoveDuplicatesFromSorterArray.java
@@ -0,0 +1,14 @@
+package LeetCode;
+
+public class RemoveDuplicatesFromSorterArray {
+    public static void main(String[] args) {
+        int[] nums= {0,0,1,1,1,2,2,3,3,4};
+
+        System.out.println(removeDuplicates(nums));
+    }
+
+
+    public static int removeDuplicates(int[] nums) {
+
+    }
+}
