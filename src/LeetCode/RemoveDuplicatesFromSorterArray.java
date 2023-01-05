89719997b62cb1ae0053bb251f08f4f2b1500872
Kramer Brown <brownkramer63@gmail.com>
2023-01-05 15:23:36 -0600

    selfpractice

--- src/LeetCode/RemoveDuplicatesFromSorterArray.java
+++ src/LeetCode/RemoveDuplicatesFromSorterArray.java
@@ -1,5 +1,7 @@
 package LeetCode;
 
+import java.util.ArrayList;
+
 public class RemoveDuplicatesFromSorterArray {
     public static void main(String[] args) {
         int[] nums= {0,0,1,1,1,2,2,3,3,4};
@@ -9,6 +11,18 @@ public class RemoveDuplicatesFromSorterArray {
 
 
     public static int removeDuplicates(int[] nums) {
-
+        // Length of the updated array
+        int count = 0;
+        // Loop for all the elements in the array
+        for (int i = 0; i < nums.length; i++) {
+            // If the current element is equal to the next element, we skip
+            if (i < nums.length - 1 && nums[i] == nums[i + 1]) {
+                continue;
+            }
+            // We will update the array in place
+            nums[count] = nums[i];
+            count++;
+        }
+        return count;
     }
 }
