60c29ce902901edd6fab0d068f739041f67e8aa5
Kramer Brown <brownkramer63@gmail.com>
2023-01-09 16:33:56 -0600

    selfpractice

---
+++ src/LocalData.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LocalData {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter df= DateTimeFormatter.ofPattern("y MMMM");
+
+        System.out.println(today.format(df));
+    }
+}
