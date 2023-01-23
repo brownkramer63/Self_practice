c332515d1fc05bc224714237a0cb5c9e0ee9a289
Kramer Brown <brownkramer63@gmail.com>
2023-01-23 15:06:41 -0600

    selfpractice

--- src/edabit/FixBasicCalculator.java
+++ src/edabit/FixBasicCalculator.java
@@ -1,4 +1,16 @@
 package edabit;
 
 public class FixBasicCalculator {
+    public static void main(String[] args) {
+
+    }
+    public static Object basicCalculator(int a,String o,int b) {
+        int result = 0;
+        if(o == "+") return a + b;
+        if(o == "-") return a - b;
+        if(o == "/" ) return a / b;
+        if(b == 0) return null;
+        if(o == "*") return a * b;
+        return result;
+    }
 }
