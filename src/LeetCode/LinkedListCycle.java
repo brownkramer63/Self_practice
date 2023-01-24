bc74f2bc9d43505c6495fa3424c6bed707aa72af
Kramer Brown <brownkramer63@gmail.com>
2023-01-24 15:02:14 -0600

    selfpractice

--- src/LeetCode/LinkedListCycle.java
+++ src/LeetCode/LinkedListCycle.java
@@ -1,8 +1,31 @@
 package LeetCode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LinkedListCycle {
+
     public static void main(String[] args) {
 
     }
+    public static boolean hasCycle(ListNode head){
+        Set<ListNode> nodesSeen = new HashSet<>();
+        while (head != null) {
+            if (nodesSeen.contains(head)) {
+                return true;
+            } else {
+                nodesSeen.add(head);
+            }
+            head = head.next;
+        }
+        return false;
+    }
 
 }
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }}
