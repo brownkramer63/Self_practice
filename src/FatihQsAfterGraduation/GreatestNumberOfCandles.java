7f9d0fed8ddedeee500949611b4e306f175dc8bb
Kramer Brown <brownkramer63@gmail.com>
2023-02-06 10:44:06 -0600

    selfpractice

---
+++ src/FatihQsAfterGraduation/GreatestNumberOfCandles.java
@@ -0,0 +1,14 @@
+package FatihQsAfterGraduation;
+
+import java.lang.reflect.Array;
+
+public class GreatestNumberOfCandles {
+    public static void main(String[] args) {
+
+    }
+    public static boolean[] greatestCandies(int[] arr, int extras){
+
+
+
+    }
+}
