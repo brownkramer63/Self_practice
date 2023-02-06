b85f2fd70731b441d5cf949f7db9d6ec783d6a24
Kramer Brown <brownkramer63@gmail.com>
2023-02-06 11:06:59 -0600

    selfpractice

--- src/FatihQsAfterGraduation/GreatestNumberOfCandles.java
+++ src/FatihQsAfterGraduation/GreatestNumberOfCandles.java
@@ -1,14 +1,38 @@
 package FatihQsAfterGraduation;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GreatestNumberOfCandles {
     public static void main(String[] args) {
+        int[] arr={4,2,1,1,2};
+        int extras=1;
 
+        System.out.println(Arrays.toString(greatestCandies(arr,extras)));
+        System.out.println(arr.length);
+        System.out.println(Arrays.toString(new int[5]));
+        ArrayList<String> list = new ArrayList(Arrays.asList("Ryan", "Julie", "Bob","d","d"));
+        int example=list.size();
+        System.out.println(example);
     }
     public static boolean[] greatestCandies(int[] arr, int extras){
+        boolean[] answer = new boolean[arr.length];
+        int greatest=0;
+        for (int i = 0; i <arr.length ; i++) {
+            if (arr[i]>greatest){
+                greatest=arr[i];
+            }
+        }
+        for (int i = 0; i < arr.length ; i++) {
+         if (arr[i]+extras>=greatest){
+             answer[i]=true;
+         }else {
+             answer[i]=false;
+         }
 
+        }
 
-
+return answer;
     }
 }
