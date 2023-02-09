9407f45820e90fb4fb43c12eff071bed07042dc4
Kramer Brown <brownkramer63@gmail.com>
2023-02-09 16:11:25 -0600

    selfpractice

--- src/LeetCode/ClimbingStairs.java
+++ src/LeetCode/ClimbingStairs.java
@@ -5,7 +5,14 @@ public class ClimbingStairs {
 
     }
     public static int climbStairs(int n) {
-
-        return 0;
+        if(n <= 3){
+            return n;
+        }
+        int a = 3, b = 2;
+        for(int i = 0; i < n-3; i++){
+            a = a + b;
+            b = a - b;
+        }
+        return a;
     }
 }
