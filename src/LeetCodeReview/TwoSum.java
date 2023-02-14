785d98c20f9b86673b033ca76f22b767282f76d0
Kramer Brown <brownkramer63@gmail.com>
2023-02-13 22:15:12 -0600

    selfpractice

--- src/LeetCodeReview/TwoSum.java
+++ src/LeetCodeReview/TwoSum.java
@@ -1,14 +1,18 @@
 package LeetCodeReview;
 
+import java.util.Arrays;
+
 public class TwoSum {
     public static void main(String[] args) {
-
+        int[] arr={3,3};
+        int target=6;
+        System.out.println(Arrays.toString(twoSum(arr,target)));
     }
-    public int[] twoSum(int[] arr, int target){
+    public static int[] twoSum(int[] arr, int target){
         int[] answer = new int[]{0, 0};
         for(int i=0; i<arr.length;i++){
-            for(int j=0; j<arr.length; j++){
-                if(arr[i]!=arr[j] && (arr[i]+arr[j])==target){
+            for(int j=1; j<arr.length; j++){
+                if(i!=j && (arr[i]+arr[j])==target){
                     return answer= new int[]{i,j};
                 }
 
