5768805fa9714fbc3ad9af5ed1f3dd9fa12bf7f8
Kramer Brown <brownkramer63@gmail.com>
2023-02-15 21:53:44 -0600

    selfpractice

---
+++ src/CodingBatREview/WithoutTen.java
@@ -0,0 +1,30 @@
+package CodingBatREview;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class WithoutTen {
+    public static void main(String[] args) {
+int[] nums={1,10,10,2};
+        System.out.println(Arrays.toString(withoutTen(nums)));
+    }
+    public static int[] withoutTen(int[] nums) {
+        ArrayList<Integer> arr= new ArrayList<Integer>(nums.length);
+        for (Integer each:nums
+             ) {
+            if (each!=10){
+                arr.add(each);
+            }
+        }
+        int[] arr1= new int[nums.length];
+        for (int i = 0; i < arr1.length ; i++) {
+           if (i<arr.size()){
+               arr1[i]=arr.get(i);
+           }else {
+               arr1[i]=0;
+           }
+        }
+        return arr1;
+    }
+}
