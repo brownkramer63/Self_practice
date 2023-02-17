15d3df741755f4e4bec00a40d0a19132c1bf37d2
Kramer Brown <brownkramer63@gmail.com>
2023-02-17 16:44:19 -0600

    selfpractice

---
+++ src/LeetCode/IsValid.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class IsValid {
+    public static void main(String[] args) {
+
+    }
+    public static int findMedian(List<Integer> arr) {
+        // Write your code here
+       arr=arr.stream().sorted().collect(Collectors.toList());
+      return 0;
+    }
+}
