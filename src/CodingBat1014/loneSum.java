815109e9ce6625a6dbeaf9d52e42746667ad3ff8
Kramer Brown <brownkramer63@gmail.com>
2023-02-18 15:10:02 -0600

    selfpractice

--- src/CodingBat1014/loneSum.java
+++ src/CodingBat1014/loneSum.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class loneSum {
     public static void main(String[] args) {
 
         System.out.println(loneSum(3,2,3));
+        System.out.println(evenlySpaced(4,6,2));
     }
     public static int loneSum(int a, int b, int c) {
         int answer = 0;
@@ -32,5 +34,15 @@ public class loneSum {
         }
 return answer;
     }
-
+    public static boolean evenlySpaced(int a, int b, int c) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(a);
+        arr.add(b);
+        arr.add(c);
+        arr= (ArrayList<Integer>) arr.stream().sorted().collect(Collectors.toList());
+        if (arr.get(2)-arr.get(1)== arr.get(1)-arr.get(0)){
+            return true;
+        }
+        return false;
+    }
 }
