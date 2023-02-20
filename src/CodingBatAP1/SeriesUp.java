891789664f9bd4c4f1ccb7505312612101d75b91
Kramer Brown <brownkramer63@gmail.com>
2023-02-20 12:45:45 -0600

    selfpractice

--- src/CodingBatAP1/SeriesUp.java
+++ src/CodingBatAP1/SeriesUp.java
@@ -19,5 +19,40 @@ public class SeriesUp {
         Object[] obj= arr.toArray();
         return obj;
        }
+    public int countClumps(int[] nums) {
+        int holder=0;
+        for(int i=1; i<nums.length-1; i++){
+
+            if(nums[i]==nums[i-1] || nums[i]==nums[i+1]){
+                holder++;
+            }
+
+        }
+        return holder/2;
+    }
+    public boolean scores100(int[] scores) {
+        for(int i=1; i<scores.length; i++){
+            if(scores[i]==100 && scores[i-1]==100){
+                return true;
+            }
+            if(scores[i]==100 && scores[i+1]==100){
+                return true;
+            }
+        }
+        return false;
+    }
+    public int scoreUp(String[] key, String[] answers) {
+        int score =0;
+        for (int i = 0; i < key.length ; i++) {
+            if (key[i].equals(answers[i])){
+                score=score+4;
+            } else if ((!answers[i].equals(key[i])) && (!answers[i].equals("?"))) {
+                score=score-1;
+            }
+
+
+        }
+        return score;
+    }
     }
 
