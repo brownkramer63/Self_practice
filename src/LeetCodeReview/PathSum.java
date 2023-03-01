d3b88cdfa83278d32b2199597f4ee804ff3e1c7a
Kramer Brown <brownkramer63@gmail.com>
2023-02-28 21:27:15 -0600

    selfpractice

---
+++ src/LeetCodeReview/PathSum.java
@@ -0,0 +1,21 @@
+package LeetCodeReview;
+
+public class PathSum {
+    public static void main(String[] args) {
+
+    }
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+return true;
+    }
+      public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+}}
