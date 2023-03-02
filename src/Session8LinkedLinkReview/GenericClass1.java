0eceb91766a119737f39c2eed6fc232ba758a8c0
Kramer Brown <brownkramer63@gmail.com>
2023-03-02 12:25:32 -0600

    selfpractice

--- src/Session8LinkedLinkReview/GenericClass1.java
+++ src/Session8LinkedLinkReview/GenericClass1.java
@@ -1,5 +1,8 @@
 package Session8LinkedLinkReview;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericClass1<T> {
 
     private T t;
@@ -9,5 +12,11 @@ public class GenericClass1<T> {
     }
     public static <T> boolean isEqual(GenericClass1<T> g1, GenericClass1<T> g2){
         return g1.getT().equals(g2.getT());
+    }
+    public List<Object> sum(GenericClass1<T> g1, GenericClass1<T> g2){
+        ArrayList<Object> arr = new ArrayList<>();
+        arr.add(g1);
+        arr.add(g2);
+        return arr;
     }
 }
