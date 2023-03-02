f859f4dfef51967c2af1dc52889fbd43da548b19
Kramer Brown <brownkramer63@gmail.com>
2023-03-02 12:47:58 -0600

    selfpractice

--- src/Session8LinkedLinkReview/GenericClass1.java
+++ src/Session8LinkedLinkReview/GenericClass1.java
@@ -13,10 +13,16 @@ public class GenericClass1<T> {
     public static <T> boolean isEqual(GenericClass1<T> g1, GenericClass1<T> g2){
         return g1.getT().equals(g2.getT());
     }
-    public List<Object> sum(GenericClass1<T> g1, GenericClass1<T> g2){
+    public List<Object> sumlist(GenericClass1<T> g1, GenericClass1<T> g2){
         ArrayList<Object> arr = new ArrayList<>();
         arr.add(g1);
         arr.add(g2);
         return arr;
     }
+    public static Long sum(GenericClass1<? extends Number> num1, GenericClass1<? extends Number> num2){
+        return num1.getT().longValue() + num2.getT().longValue();
+    }
+
+
+
 }
