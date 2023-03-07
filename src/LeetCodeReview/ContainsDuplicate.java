0e785f5987824c85d183b4b7ba8076ff728c862e
Kramer Brown <brownkramer63@gmail.com>
2023-03-06 19:31:24 -0600

    selfpractice

---
+++ src/LeetCodeReview/ContainsDuplicate.java
@@ -0,0 +1,19 @@
+package LeetCodeReview;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+    int[] arr = {1,2,3,4};
+        System.out.println(containsDuplicate(arr));
+    }
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i+1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
