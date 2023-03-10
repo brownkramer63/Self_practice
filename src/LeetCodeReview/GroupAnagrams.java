11ebe4589fb6cc7dd0dc5f6d4989fe54fc89ad94
Kramer Brown <brownkramer63@gmail.com>
2023-03-10 16:40:37 -0600

    selfpractice

--- src/LeetCodeReview/GroupAnagrams.java
+++ src/LeetCodeReview/GroupAnagrams.java
@@ -1,7 +1,6 @@
 package LeetCodeReview;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class GroupAnagrams {
     public static void main(String[] args) {
@@ -10,6 +9,35 @@ public class GroupAnagrams {
     }
     public List<List<String>> groupAnagrams(String[] strs) {
 
-        return new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+
+        return new AbstractList<List<String>>(){
+
+            List<List<String>> result;
+            public List<String> get(int index) {
+                if (result == null) init();
+                return result.get(index);
+            }
+
+            public int size() {
+                if (result == null) init();
+                return result.size();
+            }
+
+            private void init() {
+                for (String s: strs) {
+                    char[] keys = new char[26];
+                    for (int i = 0; i < s.length(); i++)
+                        keys[s.charAt(i) - 'a']++;
+
+                    String key = new String(keys);
+                    System.out.println(key);
+                    List<String> list = map.get(key);
+                    if (list == null) map.put(key, new ArrayList<>());
+                    map.get(key).add(s);
+                }
+                result = new ArrayList<>(map.values());
+            }
+        };
     }
 }
