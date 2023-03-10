ebee77574de004f091cec6d333199763050494ac
Kramer Brown <brownkramer63@gmail.com>
2023-03-09 19:49:12 -0600

    selfpractice

---
+++ src/LeetCodeReview/GroupAnagrams.java
@@ -0,0 +1,15 @@
+package LeetCodeReview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+
+
+    }
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        return new ArrayList<>();
+    }
+}
