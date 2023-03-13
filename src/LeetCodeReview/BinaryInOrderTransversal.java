55ba4ddf683f91c027229125ca993dbb9d631057
Kramer Brown <brownkramer63@gmail.com>
2023-03-12 21:25:31 -0500

    selfpractice

--- src/LeetCodeReview/BinaryInOrderTransversal.java
+++ src/LeetCodeReview/BinaryInOrderTransversal.java
@@ -1,8 +1,21 @@
 package LeetCodeReview;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinaryInOrderTransversal {
     public static void main(String[] args) {
 
     }
-    
-}
+    public List<Integer> inorderTraversal(ConvertSortedArraytoBinarySearchTree.TreeNode root) {
+        List<Integer> arr = new ArrayList<>();
+
+        if (root != null) {
+            inorderTraversal(root.left);
+            arr.add(root.val);
+            inorderTraversal(root.right);
+        }
+        return arr;
+    }}
