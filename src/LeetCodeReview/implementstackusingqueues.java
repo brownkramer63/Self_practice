284b22895032e0c1ecdfd898dc0148f1c4927f69
Kramer Brown <brownkramer63@gmail.com>
2023-03-14 15:41:44 -0500

    selfpractice

---
+++ src/LeetCodeReview/implementstackusingqueues.java
@@ -0,0 +1,27 @@
+package LeetCodeReview;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class implementstackusingqueues {
+    public static void main(String[] args) {
+        Queue<Integer> q1 = new LinkedList<>();
+    }
+
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public boolean empty() {
+
+    }
+}
