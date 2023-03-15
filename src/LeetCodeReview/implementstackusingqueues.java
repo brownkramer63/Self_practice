ddbc387d2cec059d56f8ce288688ccbba61eaff2
Kramer Brown <brownkramer63@gmail.com>
2023-03-15 15:45:47 -0500

    selfpractice

--- src/LeetCodeReview/implementstackusingqueues.java
+++ src/LeetCodeReview/implementstackusingqueues.java
@@ -8,20 +8,46 @@ public class implementstackusingqueues {
         Queue<Integer> q1 = new LinkedList<>();
     }
 
+//
+//    public void push(int x) {
+//
+//    }
+//
+//    public int pop() {
+//
+//    }
+//
+//    public int top() {
+//
+//    }
+//
+//    public boolean empty() {
+//
+//    }
+//    class MyStack {
+//
+//        private Queue<Integer> queue = new LinkedList<>();
+//
+//        public void push(int x) {
+//            queue.add(x);
+//            for(int i = 1;i<queue.size();i++) {
+//                queue.add(queue.remove());
+//            }
+//        }
+//
+//        public int pop() {
+//            return queue.remove();
+//        }
+//
+//        public int top() {
+//            return queue.peek();
+//        }
+//
+//        public boolean empty() {
+//            return queue.isEmpty();
+//        }
+//    }
 
-    public void push(int x) {
 
-    }
 
-    public int pop() {
-
-    }
-
-    public int top() {
-
-    }
-
-    public boolean empty() {
-
-    }
 }
