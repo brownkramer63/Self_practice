73bcd854e241a9ce9c4dcf3111ae3200e4128831
Kramer Brown <brownkramer63@gmail.com>
2023-03-21 13:07:37 -0500

    selfpractice

--- src/Session8LinkedLinkReview/SumNumbers.java
+++ src/Session8LinkedLinkReview/SumNumbers.java
@@ -4,26 +4,32 @@ import java.util.ArrayList;
 
 public class SumNumbers {
     public static void main(String[] args) {
-    String str="abc123xyz";
+    String str="aa11b33";
         System.out.println(sumNumbers(str));
     }
     public static int sumNumbers(String str) {
-        String s="";
-        ArrayList<Integer>arr = new ArrayList<>();
-        for (Character c:str.toCharArray()
-             ) {
-            if (c=='0' || c=='1' || c=='2'|| c=='3'|| c=='4'|| c=='5'|| c=='6'|| c=='7'|| c=='8'|| c=='9' ){
-                s=s+(c+"");
-            }else {
-               int a =Integer.parseInt(s);
-               arr.add(a);
-               s="";
+        String s = "";
+        ArrayList<Integer> arr = new ArrayList<>();
+        char[] cc = str.toCharArray();
+        for (int i = 0; i < cc.length; i++) {
+            if (Character.isDigit(cc[i])) {
+                s += cc[i];
+            } else {
+                if (s != "") {
+                    int t = Integer.parseInt(s);
+                    arr.add(t);
+                    s = "";
+                }
             }
         }
-        int answer=0;
-        for (Integer each:arr
-             ) {
-            answer+=each;
+        if (!s.isEmpty()){
+            int b = Integer.parseInt(s);
+            arr.add(b);
+        }
+        int answer = 0;
+        for (Integer each : arr
+        ) {
+            answer += each;
         }
         return answer;
     }
