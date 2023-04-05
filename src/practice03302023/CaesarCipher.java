be025bbeb4e2473854149519089bbe8bd14ed6fb
Kramer Brown <brownkramer63@gmail.com>
2023-04-05 16:57:40 -0500

    selfpractice

---
+++ src/practice03302023/CaesarCipher.java
@@ -0,0 +1,11 @@
+package practice03302023;
+
+public class CaesarCipher {
+    public static void main(String[] args) {
+
+    }
+    public static String caesarCipher(String s, int k) {
+        // Write your code here
+    return "";
+    }
+}
---
+++ src/practice03302023/GridChallenge.java
@@ -0,0 +1,13 @@
+package practice03302023;
+
+import java.util.List;
+
+public class GridChallenge {
+    public static void main(String[] args) {
+
+    }
+    public static int superDigit(String n, int k) {
+        // Write your code here
+
+    }
+}
