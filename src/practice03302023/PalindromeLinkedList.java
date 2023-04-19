d3fc0056616e2c9ad5acc72bcfe03f2fabc7fb2f
Kramer Brown <brownkramer63@gmail.com>
2023-04-18 20:54:07 -0500

    selfpractice

--- src/practice03302023/PalindromeLinkedList.java
+++ src/practice03302023/PalindromeLinkedList.java
@@ -1,4 +1,26 @@
 package practice03302023;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class PalindromeLinkedList {
+    public static void main(String[] args) {
+        LinkedList<ListNode> list = new LinkedList<>();
+
+    }
+    public  static boolean isPalindrome(ListNode head){
+        Stack<Integer> stck = new Stack<>();
+        ListNode top= head;
+        while (top.next!=null){
+            stck.push(top.val);
+            top=top.next;
+        }
+        while (!stck.isEmpty()){
+        if (head.val!=stck.pop()){
+            return false;
+        }
+        head=head.next;
+        }
+        return true;
+    }
 }
