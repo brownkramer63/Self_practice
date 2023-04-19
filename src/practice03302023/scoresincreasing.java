b3918eff7068174b886a0e1cf827d2cb90372b7a
Kramer Brown <brownkramer63@gmail.com>
2023-04-19 16:42:55 -0500

    selfpractice

--- src/practice03302023/scoresincreasing.java
+++ src/practice03302023/scoresincreasing.java
@@ -13,4 +13,15 @@ public class scoresincreasing {
         }
         return true;
     }
+    public static boolean scores100(int[] scores) {
+        for (int i = 1; i <scores.length-1 ; i++) {
+            if (scores[i-1]==100 && scores[i]==100){
+                return true;
+            }
+            if (scores[i+1]==100 && scores[i]==100){
+                return true;
+            }
+        }
+        return false;
+    }
 }
