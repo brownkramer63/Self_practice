0f75ac13893838515ef47235609b7bb727dc57ca
Kramer Brown <brownkramer63@gmail.com>
2023-04-20 13:48:08 -0500

    selfpractice

---
+++ src/practice03302023/assesment1.java
@@ -0,0 +1,13 @@
+package practice03302023;
+
+public class assesment1 {
+    public static double average(int a, int b) {
+        int answer=(a + b) / 2;
+        double aa = (double) answer;
+        return aa;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(average(2,1));
+    }
+}
